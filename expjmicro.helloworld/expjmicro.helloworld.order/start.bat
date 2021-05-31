2114a44ee5cc877a15554d0c7199e9156d2829d5
mynewworldyyl <mynewworldyyl@gmail.com>
2021-05-31 21:24:23 +0800

    版本更新

--- expjmicro.helloworld/expjmicro.helloworld.order/start.bat
+++ expjmicro.helloworld/expjmicro.helloworld.order/start.bat
@@ -1,6 +1,6 @@
 title expjmicro.helloworld.order
 java ^
 -Xbootclasspath/a:%MAVEN_RESP_HOME%\org\javassist\javassist\3.24.0-GA\javassist-3.24.0-GA.jar ^
--javaagent:%MAVEN_RESP_HOME%\cn\jmicro\jmicro.agent\0.0.3-RELEASE\jmicro.agent-0.0.3-RELEASE.jar ^
--jar  %MAVEN_RESP_HOME%\cn\expjmicro\expjmicro.helloworld.order\0.0.3-RELEASE\expjmicro.helloworld.order-0.0.3-RELEASE-with-core.jar ^
+-javaagent:%MAVEN_RESP_HOME%\cn\jmicro\jmicro.agent\0.0.5-SNAPSHOT\jmicro.agent-0.0.5-SNAPSHOT.jar ^
+-jar  %MAVEN_RESP_HOME%\cn\expjmicro\expjmicro.helloworld.order\0.0.5-SNAPSHOT\expjmicro.helloworld.order-0.0.5-SNAPSHOT-with-core.jar ^
 -DsysLogLevel=2  -DclientId=25500 -DadminClientId=0  -Dlog4j.configuration=%JMICRO_DEMO_HOME%\log4j.xml -Dpwd=1
